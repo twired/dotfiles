e0dc80f87312fee8e3f498d1828be5fc6c406c5a
twired <imetrbl@gmail.com>
2025-05-10 22:18:05 +0200

    i3 and polybar

--- .config/.config/polybar/bluetooth.sh
+++ .config/.config/polybar/bluetooth.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo "%{F#ffffff}BLUETOOTH"
+  echo "%{F#ffffff}BT: OFF"
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
   then 
-    echo "BLUETOOTH"
+    echo "BT"
   fi
-  echo "%{F#ffffff}BLUETOOTH"
+  echo "%{F#ffffff}BT: ON"
 fi
 
